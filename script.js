d2baa2e89f244b1dea379ec27d264eaeb5085396
Marta Piotrowska <m.piotrowska.257@studms.ug.edu.pl>
2025-09-23 17:31:14 +0200

    implement task management features: add task editing, deletion, and counters for completed and uncompleted tasks + fixing issues in html code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-const li = document.createElement("li");
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
 
 function addTask() {
+  const li = document.createElement("li");
   const task = inputBox.value.trim();
   if (!task) {
     alert("Please write down a task");
@@ -20,4 +22,42 @@ function addTask() {
 
   listContainer.appendChild(li);
   inputBox.value = "";
+
+  const checkbox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const taskSpan = li.querySelector("span");
+  const deleteBtn = li.querySelector(".delete-btn");
+
+  checkbox.addEventListener("click", function () {
+    li.classList.toggle("completed", checkbox.checked);
+    updateCounters();
+  });
+
+  editBtn.addEventListener("click", function () {
+    const update = prompt("Edit task:", taskSpan.textContent);
+    if (update !== null) {
+      taskSpan.textContent = update;
+      li.classList.remove("completed");
+      checkbox.checked = false;
+      updateCounters();
+    }
+  });
+
+  deleteBtn.addEventListener("click", function () {
+    if (confirm("Are you sure you want to delete this task?")) {
+      li.remove();
+      updateCounters();
+    }
+  });
 }
+
+function updateCounters() {
+  const completedTasks = document.querySelectorAll(".completed").length;
+  const uncompletedTasks =
+    document.querySelectorAll("li:not(.completed").length;
+
+  completedCounter.textContent = completedTasks;
+  uncompletedCounter.textContent = uncompletedTasks;
+}
+
+updateCounters();
